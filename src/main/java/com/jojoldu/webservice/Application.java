69c69025552fc0d5b8404b2f0eab12afd0d113f7
jikimee64 <jikimee64@gmail.com>
2020-03-17 23:14:11 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/webservice/Application.java
@@ -0,0 +1,9 @@
+package com.jojoldu.webservice;
+public class Application {
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/test/java/com/jojoldu/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.jojoldu.webservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+}
